8f147489fba5968242a1199c351d344753d71e38
nouhhasan1-hub <nouh.hasan1@gmail.com>
2026-01-23 13:56:17 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Network-Information-Lookup-Tool-">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
